3e90eb568d07a36185ed539733ff9c938b45f346
Flore Perche <flore.perche@gmail.com>
2024-03-08 10:02:47 -0300

    d3 + data imported

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,34 @@
 <script>
+  import data from "./assets/pantheon_data.json";
+  const margin = { top: 10, right: 100, left: 100, bottom: 30 };
+
+  let width = 800;
+  let height = 600;
 </script>
 
-<main></main>
+<main>
+  <div class="intro">
+    <h1>Aux grandes femmes, la Nation reconnaissante?</h1>
+    <p>Dataviz</p>
+  </div>
+  <div class="graph-container">
+    <svg {width} {height}></svg>
+  </div>
+</main>
 
 <style>
+  .intro {
+    font-family: "Cabinet Grotesk", sans-serif;
+    text-align: center;
+  }
+
+  .graph-container {
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+  }
+
+  svg {
+    background-color: rgb(243, 245, 222);
+  }
 </style>
