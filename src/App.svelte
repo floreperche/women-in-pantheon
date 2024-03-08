9c6fa212a3eabb33efb0feb86710a3b156dc4da4
Flore Perche <flore.perche@gmail.com>
2024-03-08 17:12:10 -0300

    working spiral

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,37 @@
 <script>
   import data from "./assets/pantheon_data.json";
+
   const margin = { top: 10, right: 100, left: 100, bottom: 30 };
 
-  let width = 800;
-  let height = 600;
+  let width = 500;
+  let height = 500;
+
+  // Spiral viz
+  const radius = width / 2 - 20;
+  // Number of spirals
+  const coils = 5.6;
+  const rotation = 2 * Math.PI;
+  const thetaMax = coils * 1.5 * Math.PI;
+  const awayStep = radius / thetaMax;
+  // Spacing
+  const chord = 36;
+  let theta = chord / awayStep;
+  // let initialData = data.sort((a, b) => a.grade - b.grade);
+  $: processedData = [];
+  $: data
+    .sort((a, b) => a.transfered_date - b.transfered_date)
+    .map((e) => {
+      if (theta <= thetaMax) {
+        let away = awayStep * theta;
+        let around = theta + rotation;
+        let x = width / 2 + Math.cos(around) * away;
+        let y = height / 2 + Math.sin(around) * away;
+        theta += chord / away;
+        e["x"] = x;
+        e["y"] = y;
+        processedData.push(e);
+      }
+    });
 </script>
 
 <main>
@@ -11,8 +39,21 @@
     <h1>Aux grandes femmes, la Nation reconnaissante?</h1>
     <p>Dataviz</p>
   </div>
-  <div class="graph-container">
-    <svg {width} {height}></svg>
+
+  <div class="graph-container second">
+    <svg {height} {width}>
+      <g>
+        {#if processedData}
+          {#each processedData as person}
+            <circle
+              cx={person.x}
+              cy={person.y}
+              r="16"
+              fill={person.sex === "M" ? "#1B2B5C" : "#F34C63"}
+            />{/each}
+        {/if}</g
+      ></svg
+    >
   </div>
 </main>
 
@@ -26,9 +67,5 @@
     margin: 0 auto;
     display: flex;
     justify-content: center;
-  }
-
-  svg {
-    background-color: rgb(243, 245, 222);
   }
 </style>
