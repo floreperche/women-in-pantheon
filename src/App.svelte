0084aa8a3f814fb1b36630ec8e494d0e32ad9eb3
Flore Perche <flore.perche@gmail.com>
2024-03-11 18:18:16 -0300

    Axes and legends of the beeswarm chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,16 @@
 <script>
   import data from "./assets/pantheon_data.json";
-  import { forceSimulation, forceX, forceY, forceCollide } from "d3";
-  import { scaleLinear, scaleBand } from "d3";
-  import SpiralViz from "./components/SpiralViz.svelte";
-  import Flower from "./components/Flower.svelte";
 
-  // const margin = { top: 10, right: 100, left: 100, bottom: 30 };
+  import SpiralViz from "./components/SpiralViz.svelte";
+  import BeeSwarmViz from "./components/BeeSwarmViz.svelte";
+
+  const margin = { top: 10, right: 30, left: 30, bottom: 30 };
 
   let width = 600;
   let height = 500;
   let marginHeight = 150;
 
   // Bee-swarm chart
-
-  $: xScale = scaleLinear().domain([1791, 2024]).range([0, width]);
-
-  $: yScale = scaleBand().domain(["M", "W"]).range([height, 100]);
-
-  const RADIUS = 7;
-
-  let simulation = forceSimulation(data);
-  let nodes = [];
-
-  simulation.on("tick", () => {
-    nodes = simulation.nodes();
-  });
-
-  $: {
-    simulation
-      .force(
-        "x",
-        forceX()
-          .x((d) => xScale(d.transfered_date))
-          .strength(0.8)
-      )
-      .force(
-        "y",
-        forceY()
-          .y((d) => yScale(d.sex))
-          .strength(0.2)
-      )
-      .force("collide", forceCollide().radius(RADIUS))
-      .alpha(0.3)
-      .alphaDecay(0.0005)
-      .restart();
-  }
 
   // console.log(simulation.nodes());
 </script>
@@ -57,20 +23,8 @@
 
   <SpiralViz {data} {width} {height} {marginHeight} />
 
-  <div class="graph-container bee">
-    <svg {height} {width}>
-      {#each nodes as person}
-        <circle
-          cx={person.x}
-          cy={person.y}
-          r="5"
-          fill={person.sex === "W" ? "#F34C63" : "#7480D2"}
-          stroke="#120833"
-          class={person.name}
-        />
-      {/each}
-    </svg>
-  </div>
+  <BeeSwarmViz {data} {width} {height} {margin} {marginHeight} />
+  <div class="bottom-space"></div>
 </main>
 
 <style>
@@ -82,11 +36,7 @@
     text-align: center;
   }
 
-  .graph-container {
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  .bottom-space {
+    height: 100px;
   }
 </style>
---
+++ src/components/BeeSwarmViz.svelte
@@ -0,0 +1,182 @@
+<script>
+  import timeData from "../assets/time_periods.json";
+
+  import Flower from "./Flower.svelte";
+  import {
+    forceSimulation,
+    forceX,
+    forceY,
+    forceCollide,
+    scaleOrdinal,
+  } from "d3";
+  import { scaleLinear } from "d3";
+
+  export let data;
+  export let width;
+  export let height;
+  export let margin;
+  export let marginHeight;
+
+  $: xScale = scaleLinear()
+    .domain([1791, 2024])
+    .range([0 + margin.left, width - margin.right]);
+
+  const colorRange = ["#F34C63", "#7480D2"];
+  let colorScale = scaleOrdinal().domain(["W", "M"]).range(colorRange);
+
+  const RADIUS = 8;
+
+  let simulation = forceSimulation(data);
+  let nodes = [];
+
+  simulation.on("tick", () => {
+    nodes = simulation.nodes();
+  });
+
+  $: {
+    simulation
+      .force(
+        "x",
+        forceX()
+          .x((d) => xScale(d.transfered_date))
+          .strength(0.9)
+      )
+      .force(
+        "y",
+        forceY()
+          .y((d) => height / 2)
+          .strength(0.05)
+      )
+      .force("collide", forceCollide().radius(RADIUS))
+      .alpha(0.2)
+      .alphaDecay(0.005)
+      .restart();
+  }
+
+  // Ticks
+  let xTicks = [1800, 1850, 1900, 1950, 2000];
+</script>
+
+<div class="graph-container bee">
+  <!-- Legend -->
+  <div class="legend">
+    <svg {width} height={marginHeight}>
+      <g
+        transform="translate({width / 2 - 200} {marginHeight / 2 -
+          10}), scale({0.7})"
+        ><Flower person={{ sex: "W", status: "Panthéonisée" }} /><text
+          fill="#EEE7EF"
+          x="0"
+          y="60"
+          text-anchor="middle"
+          transform="scale({1})"
+          ><tspan x="0" dy="-0.5em" text-anchor="middle">Femme</tspan><tspan
+            x="0"
+            dy="1.2em"
+            text-anchor="middle">panthéonisée</tspan
+          >
+        </text></g
+      >
+      <g
+        transform="translate({width / 2 - 100} {marginHeight / 2 -
+          10}), scale({0.7})"
+        ><Flower person={{ sex: "M", status: "Panthéonisée" }} /><text
+          fill="#EEE7EF"
+          x="0"
+          y="60"
+          text-anchor="middle"
+          transform="scale({1})"
+          ><tspan x="0" dy="-0.5em" text-anchor="middle">Homme</tspan><tspan
+            x="0"
+            dy="1.2em"
+            text-anchor="middle">panthéonisé</tspan
+          ></text
+        ></g
+      >
+      <g
+        transform="translate({width / 2 + 150} {marginHeight / 2 -
+          10}), scale({0.7})"
+      >
+        <g transform="translate(-60 0)">
+          <Flower person={{ sex: "W", status: "Partenaire" }} />
+        </g>
+
+        <g transform="translate(60 0)">
+          <Flower person={{ sex: "M", status: "Partenaire" }} /></g
+        >
+        <text
+          fill="#EEE7EF"
+          x="0"
+          y="60"
+          text-anchor="middle"
+          transform="scale({1})"
+        >
+          <tspan x="0" dy="-0.5em" text-anchor="middle"
+            >Femme / homme reposant au Panthéon,</tspan
+          ><tspan x="0" dy="1.2em" text-anchor="middle"
+            >sans être panthéonisé(e)</tspan
+          >
+        </text></g
+      >
+    </svg>
+  </div>
+
+  <svg {height} {width}>
+    <!-- Axes, Gridlines and labels -->
+    {#each timeData as period}
+      <rect
+        x={xScale(period.start_date)}
+        y="0"
+        width={xScale(period.end_date) - xScale(period.start_date)}
+        {height}
+        fill="#eee7ef"
+        opacity={period.id % 2 ? "20%" : "8%"}
+      />
+    {/each}
+    {#each xTicks as tick, i}
+      <g class="tick" transform="translate({xScale(tick)},0)">
+        <text
+          fill={i % 2 ? "none" : "#EEE7EF"}
+          x="0"
+          y={height - 10}
+          text-anchor="middle">{tick}</text
+        >
+        <line
+          x1="0"
+          x2="0"
+          y1={height - 30}
+          y2={height - 40}
+          stroke="hsla(215,15%,91%,1"
+        />
+      </g>
+    {/each}
+
+    <!-- Data points -->
+    {#each nodes as person}
+      <g transform="translate({person.x} {person.y}), scale({1})"
+        ><path
+          fill={person.status === "Panthéonisée"
+            ? colorScale(person.sex)
+            : "none"}
+          stroke={person.status != "Panthéonisée"
+            ? colorScale(person.sex)
+            : "#120833"}
+          stroke-width="10"
+          d="M48.1,-66.9C63,-55.4,76.2,-42.2,82.6,-26C89,-9.8,88.5,9.4,81.8,25.5C75,41.5,62,54.4,47.3,62.6C32.6,70.8,16.3,74.3,0.2,74C-15.9,73.7,-31.7,69.6,-43.7,60.6C-55.7,51.5,-63.9,37.4,-71,21.6C-78.2,5.8,-84.4,-11.8,-81.9,-28.7C-79.3,-45.6,-68,-61.8,-52.9,-73.2C-37.8,-84.6,-18.9,-91.3,-1.1,-89.7C16.6,-88.2,33.2,-78.4,48.1,-66.9Z"
+          transform="scale(0.1)"
+          fill-opacity={person.status === "Panthéonisée" ? "100%" : "80%"}
+        />
+      </g>
+    {/each}
+  </svg>
+</div>
+
+<style>
+  .graph-container {
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
